b99151dd68af69367993c963a768a7eab244dcf5
Gabriel <gabrielbarrosguimaraes@live.com>
2023-04-17 21:28:39 -0400

    commit temporario

---
+++ MVC/ViewProject/FormProduto.Designer.cs
@@ -0,0 +1,230 @@
+﻿
+namespace ViewProject
+{
+    partial class FormNotaEntrada
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.lblDescrição = new System.Windows.Forms.Label();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.lblPrecoVenda = new System.Windows.Forms.Label();
+            this.txtVenda = new System.Windows.Forms.TextBox();
+            this.btnNovo = new System.Windows.Forms.Button();
+            this.btnGravar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.btnRemover = new System.Windows.Forms.Button();
+            this.dgvFornecedor = new System.Windows.Forms.DataGridView();
+            this.lblPrecoCusto = new System.Windows.Forms.Label();
+            this.txtCusto = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornecedor)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 20.31603F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 79.68398F));
+            this.tableLayoutPanel1.Controls.Add(this.txtCusto, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblPrecoCusto, 0, 3);
+            this.tableLayoutPanel1.Controls.Add(this.lblId, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.txtId, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblDescrição, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtDescricao, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.lblPrecoVenda, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.txtVenda, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnCancelar, 0, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnRemover, 1, 5);
+            this.tableLayoutPanel1.Controls.Add(this.btnGravar, 1, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnNovo, 0, 4);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(179, 11);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 6;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 30F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 35F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 31F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 27F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(443, 180);
+            this.tableLayoutPanel1.TabIndex = 11;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Location = new System.Drawing.Point(3, 0);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(16, 13);
+            this.lblId.TabIndex = 0;
+            this.lblId.Text = "Id";
+            // 
+            // txtId
+            // 
+            this.txtId.Enabled = false;
+            this.txtId.Location = new System.Drawing.Point(92, 3);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(348, 20);
+            this.txtId.TabIndex = 1;
+            // 
+            // lblDescrição
+            // 
+            this.lblDescrição.AutoSize = true;
+            this.lblDescrição.Location = new System.Drawing.Point(3, 28);
+            this.lblDescrição.Name = "lblDescrição";
+            this.lblDescrição.Size = new System.Drawing.Size(55, 13);
+            this.lblDescrição.TabIndex = 2;
+            this.lblDescrição.Text = "Descrição";
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(92, 31);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(348, 20);
+            this.txtDescricao.TabIndex = 3;
+            // 
+            // lblPrecoVenda
+            // 
+            this.lblPrecoVenda.AutoSize = true;
+            this.lblPrecoVenda.Location = new System.Drawing.Point(3, 56);
+            this.lblPrecoVenda.Name = "lblPrecoVenda";
+            this.lblPrecoVenda.Size = new System.Drawing.Size(83, 13);
+            this.lblPrecoVenda.TabIndex = 4;
+            this.lblPrecoVenda.Text = "Preço de venda";
+            // 
+            // txtVenda
+            // 
+            this.txtVenda.Location = new System.Drawing.Point(92, 59);
+            this.txtVenda.Name = "txtVenda";
+            this.txtVenda.Size = new System.Drawing.Size(348, 20);
+            this.txtVenda.TabIndex = 5;
+            // 
+            // btnNovo
+            // 
+            this.btnNovo.Location = new System.Drawing.Point(3, 124);
+            this.btnNovo.Name = "btnNovo";
+            this.btnNovo.Size = new System.Drawing.Size(69, 23);
+            this.btnNovo.TabIndex = 6;
+            this.btnNovo.Text = "Novo";
+            this.btnNovo.UseVisualStyleBackColor = true;
+            // 
+            // btnGravar
+            // 
+            this.btnGravar.Location = new System.Drawing.Point(92, 124);
+            this.btnGravar.Name = "btnGravar";
+            this.btnGravar.Size = new System.Drawing.Size(75, 23);
+            this.btnGravar.TabIndex = 7;
+            this.btnGravar.Text = "Gravar";
+            this.btnGravar.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Location = new System.Drawing.Point(3, 155);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(69, 22);
+            this.btnCancelar.TabIndex = 8;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            // 
+            // btnRemover
+            // 
+            this.btnRemover.Location = new System.Drawing.Point(92, 155);
+            this.btnRemover.Name = "btnRemover";
+            this.btnRemover.Size = new System.Drawing.Size(75, 22);
+            this.btnRemover.TabIndex = 9;
+            this.btnRemover.Text = "Remover";
+            this.btnRemover.UseVisualStyleBackColor = true;
+            // 
+            // dgvFornecedor
+            // 
+            this.dgvFornecedor.AllowUserToAddRows = false;
+            this.dgvFornecedor.AllowUserToDeleteRows = false;
+            this.dgvFornecedor.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvFornecedor.Location = new System.Drawing.Point(178, 197);
+            this.dgvFornecedor.Name = "dgvFornecedor";
+            this.dgvFornecedor.ReadOnly = true;
+            this.dgvFornecedor.RowHeadersWidth = 62;
+            this.dgvFornecedor.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvFornecedor.Size = new System.Drawing.Size(444, 243);
+            this.dgvFornecedor.TabIndex = 12;
+            // 
+            // lblPrecoCusto
+            // 
+            this.lblPrecoCusto.AutoSize = true;
+            this.lblPrecoCusto.Location = new System.Drawing.Point(3, 86);
+            this.lblPrecoCusto.Name = "lblPrecoCusto";
+            this.lblPrecoCusto.Size = new System.Drawing.Size(79, 13);
+            this.lblPrecoCusto.TabIndex = 13;
+            this.lblPrecoCusto.Text = "Preço de custo";
+            // 
+            // txtCusto
+            // 
+            this.txtCusto.Location = new System.Drawing.Point(92, 89);
+            this.txtCusto.Name = "txtCusto";
+            this.txtCusto.Size = new System.Drawing.Size(348, 20);
+            this.txtCusto.TabIndex = 14;
+            // 
+            // FormNotaEntrada
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.dgvFornecedor);
+            this.Name = "FormNotaEntrada";
+            this.Text = "Nota de Entrada";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvFornecedor)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.Label lblDescrição;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.Label lblPrecoVenda;
+        private System.Windows.Forms.TextBox txtVenda;
+        private System.Windows.Forms.Button btnNovo;
+        private System.Windows.Forms.Button btnGravar;
+        private System.Windows.Forms.Button btnCancelar;
+        private System.Windows.Forms.Button btnRemover;
+        private System.Windows.Forms.DataGridView dgvFornecedor;
+        private System.Windows.Forms.TextBox txtCusto;
+        private System.Windows.Forms.Label lblPrecoCusto;
+    }
+}
---
+++ MVC/ViewProject/JanelaPrincipal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ControllerProject;
+using ModelProject;
+
+
+namespace ViewProject
+{
+    public partial class FormJanelaPrincipal : Form
+    {
+        private FornecedorController fornecedorController = new FornecedorController();
+        private ProdutoController produtoController = new ProdutoController();
+
+        public FormJanelaPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void fornecedorToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            new FormFornecedor(fornecedorController).ShowDialog();
+        }
+
+    }
+}
